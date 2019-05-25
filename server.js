21cf9df7f5eb2c9ee06d0fe24d0e2123345ecb83
wwr <13432966176@163.com>
2019-05-25 13:40:11 +0800

    wenwenrui

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const compiler = webpack(config)
 const Schema = require('./src/utils/schema')
 
 app.all('*', function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,access-token");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Credentials", true);
